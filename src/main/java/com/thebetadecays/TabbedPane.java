f7e94e3cb686bbd9770489904d249fc4e507430a
Skyler <skylernovak@yahoo.com>
2019-11-29 16:23:51 -0800

    Skeleton of Expense Tab done
    still need to add event listeners, perhaps left align page

--- src/main/java/com/thebetadecays/TabbedPane.java
+++ src/main/java/com/thebetadecays/TabbedPane.java
@@ -1,10 +1,11 @@
 package com.thebetadecays;
 
+import java.awt.*;
 import javax.swing.*;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
+//import javax.swing.JFrame;
+//import javax.swing.JLabel;
+//import javax.swing.JPanel;
+//import javax.swing.JTabbedPane;
  
 /**
  * TabbedPane is the main panel that runs the GUI for Saving Cents
@@ -20,24 +21,39 @@ public class TabbedPane extends JFrame {
     private JPanel jp3 = new JPanel(); // Contacts
     private JPanel jp4 = new JPanel(); // Reports
 
+    // Add Layout Managers
+    //setLayout(new BorderLayout());
+
     // Labels
     private JLabel label1 = new JLabel(); // Dashboard Label
     private JLabel label2 = new JLabel(); // Expenses Label
     private JLabel label3 = new JLabel(); // Contacts Label
     private JLabel label4 = new JLabel(); // Reports Label
-    private JLabel testLabel = new JLabel();
+    private JLabel expConName = new JLabel();
+    private JLabel expAmt = new JLabel();
+    private JLabel expDate = new JLabel();
+    private JLabel expCatLabel = new JLabel();
 
     // Text Fields
-    private JTextField testTextField = new JTextField(25);
+    private JTextField testTextField1 = new JTextField(25);
+    private JTextField testTextField2 = new JTextField(25);
+    private JTextField testTextField3 = new JTextField(25);
 
     // Buttons
-    private JButton testButton = new JButton("Help!:");
+    private JButton expAdd = new JButton("Add");
+    private JButton expEdit = new JButton("Edit");
+    private JButton expDel = new JButton("Delete");
 
+    // Combo Boxes
+    private JComboBox expCategories;
 
     // Window params
     private final int WINDOW_WIDTH = 647;
     private final int WINDOW_HEIGHT = 400;
-    
+
+    // Categories / Sub categories string arrays
+    String categories[] = {"Housing", "Transportation", "Food", "Utilities", "Insurance", "Medical & Healthcare",
+            "Saving & Investments", "Personal", "Entertainment", "Miscellaneous"};
      
     /**
      * Constructor
@@ -48,6 +64,7 @@ public class TabbedPane extends JFrame {
         setTitle("Saving Cents");
         pack(); // Sets window large enough for all elements to appear
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);  // sets size of window (ratio is golden rectangle)
+        setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
@@ -71,18 +88,14 @@ public class TabbedPane extends JFrame {
         label3.setText("You are in area of Tab3 - CONTACTS");
         label4.setText("You are in area of Tab4 - REPORTS");
 
-        // Add labels to the correct panel
+        // Add labels to the correct panel -- Remove as panel is worked on
         jp1.add(label1);
-        jp2.add(label2);
         jp3.add(label3);
         jp4.add(label4);
 
-        // Test components      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        testLabel.setText("THIS IS A TEST 1...2...3...");
-        jp2.add(testLabel);
-        jp2.add(testTextField);
-        jp2.add(testButton);
-        
+        // add components to Expense Tab
+        buildExpenseTab();
+
         // Add panes to tabs
         jtp.addTab("Dashboard", jp1);
         jtp.addTab("Expense", jp2);
@@ -91,14 +104,46 @@ public class TabbedPane extends JFrame {
 
     } // buildPanel()
 
+    private void buildExpenseTab() {
+        jp2.setLayout(new GridLayout(7, 1));
+
+        JPanel expPanel1 = new JPanel();
+        JPanel expPanel2 = new JPanel();
+        JPanel expPanel3 = new JPanel();
+        JPanel expPanel4 = new JPanel();
+        JPanel expPanel5 = new JPanel();
+        JPanel expPanel6 = new JPanel();
+
+        expCategories = new JComboBox(categories);
+
+        expConName.setText("Contact Name");
+        expAmt.setText("Amount");
+        expDate.setText("Date");
+        expCatLabel.setText("Category");
+
+        expPanel1.add(expConName);
+        expPanel1.add(testTextField1);
+        expPanel2.add(expAmt);
+        expPanel2.add(testTextField2);
+        expPanel3.add(expDate);
+        expPanel3.add(testTextField3);
+        expPanel4.add(expCatLabel);
+        expPanel4.add(expCategories);
+        expPanel6.add(expAdd);
+        expPanel6.add(expEdit);
+        expPanel6.add(expDel);
+
+        jp2.add(expPanel1);
+        jp2.add(expPanel2);
+        jp2.add(expPanel3);
+        jp2.add(expPanel4);
+        jp2.add(expPanel5);
+        jp2.add(expPanel6);
+    }
          
     public static void main(String[] args) {
          
         TabbedPane tp = new TabbedPane();
-    
-        tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tp.pack();  // Sets window large enough for all elements to appear
-        tp.setSize(647, 400);   // sets size of window (ratio is golden rectangle)
-        tp.setVisible(true);
+
     } // Main
 } // TabbedPane
